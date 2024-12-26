87cdd3691a2720a5c4daac74d1ef397a9c3a8a4c
steven maitre <steven.maitre@gmail.com>
2024-12-26 14:45:46 +0100

    Connexion à la bdd puis installation de prisma

--- api/app.js
+++ api/app.js
@@ -1,6 +1,13 @@
 import express from "express";
+import authRoute from "./routes/auth.route.js";
+import PostRoute from "./routes/post.route.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/posts", PostRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
