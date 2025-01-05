9331a76b2f313a512b611e4aacfe76d417e16a74
steven maitre <steven.maitre@gmail.com>
2025-01-05 15:43:12 +0100

    Création des posts avec prisma vers la BDD

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import ProfilePage from "./routes/profilePage/profilePage";
 import Login from "./routes/login/login";
 import Register from "./routes/register/register";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
+import NewPostPage from "./routes/newPostPage/newPostPage";
+import { singlePageLoader } from "./lib/loaders";
 
 function App() {
   const router = createBrowserRouter([
@@ -26,7 +28,7 @@ function App() {
         {
           path: "/:id",
           element: <SinglePage />,
-        },
+          loader: singlePageLoader,
 
         {
           path: "/login",
@@ -49,6 +51,10 @@ function App() {
         {
           path: "/profile/update",
           element: <ProfileUpdatePage />,
+        },
+        {
+          path: "/add",
+          element: <NewPostPage />,
         },
       ],
     },
